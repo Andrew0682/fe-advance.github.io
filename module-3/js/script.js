2aacb6ef2b340a9578ec96fb79a7038071ca21f4
Andrey <Andrew0682@ukr.net>
2018-02-26 22:31:57 +0200

    add new method

--- module-3/js/script.js
+++ module-3/js/script.js
@@ -8,9 +8,68 @@ console.log(hello);
 console.log(javascript);
 console.log(trainer);
 
-
-
 //method 2
+const alphabet = "qwertyuiop[]asdfghjkl;'zxcvbnm,./";
+let word1 = "hello";
+let word1Arr = word1.split('');
+let word2 = "javascript";
+let word2Arr = word2.split('');
+let word3 = "trainer";
+let word3Arr = word3.split('');
+let keyboard = [];
+let result = [];
+let result1 = [];
+let result2 = [];
+let signs = alphabet.split('');
+let line1 = signs.slice(signs.indexOf('q'), signs.indexOf('a'));
+let line2 = signs.slice(signs.indexOf('a'), signs.indexOf('z'));
+let line3 = signs.slice(signs.indexOf('z'), signs.indexOf('/'));
+keyboard.push(line1);
+keyboard.push(line2);
+keyboard.push(line3);
+console.log(keyboard);
+
+for (let w=0; w<word1Arr.length; w++){
+for(let i = 0; i < keyboard.length; i++){
+    for(let j = 0; j < keyboard[i].length; j++){
+   
+      if(keyboard[i][j]==word1Arr[w]){
+  
+        result.push(keyboard[i][j])
+      }
+      
+    }
+}
+}
+console.log(result.join(""));
+for (let w=0; w<word2Arr.length; w++){
+for(let i = 0; i < keyboard.length; i++){
+    for(let j = 0; j < keyboard[i].length; j++){
+  
+      if(keyboard[i][j]==word2Arr[w]){
+  
+        result1.push(keyboard[i][j])
+      }
+      
+    }
+}
+}
+console.log(result1.join(""));
+for (let w=0; w<word3Arr.length; w++){
+for(let i = 0; i < keyboard.length; i++){
+    for(let j = 0; j < keyboard[i].length; j++){
+  
+      if(keyboard[i][j]==word3Arr[w]){
+  
+        result2.push(keyboard[i][j])
+      }
+      
+    }
+}
+}
+console.log(result2.join(""));
+
+//method 3
 const alphabet = "qwertyuiop[]asdfghjkl;'zxcvbnm,./";
 const line = alphabet.split("");
 const keyboard = [];
