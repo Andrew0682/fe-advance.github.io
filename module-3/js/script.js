e81465af5e09aae1c177e9a7bbe73bd85fb090a7
Andrey <Andrew0682@ukr.net>
2018-03-01 12:20:49 +0200

    update home

--- module-3/js/script.js
+++ module-3/js/script.js
@@ -1,29 +1,14 @@
-//method 1
 
-const alphabet = "qwertyuiop[]asdfghjkl;'zxcvbnm,./";
-const alphabetArr = [...alphabet];
-
-
-let keyboard = new Array();
-
-keyboard[0] = alphabetArr.slice(0, alphabetArr.indexOf("]") + 1);
-keyboard[1] = alphabetArr.slice(alphabetArr.indexOf("a"), alphabetArr.indexOf("'") + 1);
-keyboard[2] = alphabetArr.slice(alphabetArr.indexOf("z"));
-console.log(`${keyboard[1][5]}${keyboard[0][2]}${keyboard[1][8]}${keyboard[1][8]}${keyboard[0][8]}`);
-console.log(`${keyboard[1][6]}${keyboard[1][0]}${keyboard[2][3]}${keyboard[1][0]}${keyboard[1][1]}${keyboard[2][2]}${keyboard[0][3]}${keyboard[0][7]}${keyboard[0][9]}${keyboard[0][4]}`);
-console.log(`${keyboard[0][4]}${keyboard[0][3]}${keyboard[1][0]}${keyboard[0][7]}${keyboard[2][5]}${keyboard[0][2]}${keyboard[0][3]}`);
-
-//method 2
 
 const alphabet = "qwertyuiop[]asdfghjkl;'zxcvbnm,./";
 const alphabetArr = alphabet.split('');
 
-const keyboardUP = alphabetArr.indexOf("]")+1;
-const keyboardCenter = alphabetArr.indexOf("a");
-const keyboardDown = alphabetArr.indexOf("\'")+1;
+const firstLineEnd = alphabetArr.indexOf("]")+1;
+const secondLineStart = alphabetArr.indexOf("a");
+const secondLineEnd = alphabetArr.indexOf("\'")+1;
 
 
-const keyboard = [alphabetArr.slice(0, keyboardUP), alphabetArr.slice(keyboardCenter, keyboardDown), alphabetArr.slice(-10)];
+const keyboard = [alphabetArr.slice(0, firstLineEnd), alphabetArr.slice(secondLineStart, secondLineEnd), alphabetArr.slice(-10)];
 console.log(keyboard);
 
 const hello = `${keyboard[1][5]}${keyboard[0][2]}${keyboard[1][8]}${keyboard[1][8]}${keyboard[0][8]}`;
@@ -32,4 +17,3 @@ const trainer = `${keyboard[0][4]}${keyboard[0][3]}${keyboard[1][0]}${keyboard[0
 console.log(`${hello}
 	         ${javascript}
 	         ${trainer}`);
-
