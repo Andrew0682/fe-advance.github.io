4bf1173a88d9700b3bffd1fa938d1b26a66d6a5f
Andrey <Andrew0682@ukr.net>
2018-03-20 18:28:57 +0200

    replace home

--- module-5/js/script.js
+++ module-5/js/script.js
@@ -1,16 +1,15 @@
-const alphabetEn = "qwertyuiop[]asdfghjkl;'zxcvbnm,./";
-const alphabetRu = "йцукенгшщзхъфывапролджэячсмитьбю.";
-const alphabetUa = "йцукенгшщзхїфівапролджєячсмитьбю.";
 
-const addKeyboardLayout = (strAlphabet1, strAlphabet2, strAlphabet) => {
-    const arr = strAlphabet.split('');
-    const keyboard = [arr.slice(0, arr.indexOf(strAlphabet1)), arr.slice(arr.indexOf(strAlphabet1), arr.indexOf(strAlphabet2)), arr.slice(arr.indexOf(strAlphabet2))];
+const alphabetEn = "qwertyuiop[]asdfghjkl;'\zxcvbnm,./";
+const alphabetRu = "йцукенгшщзхъфывапролджэ\ячсмитьбю.";
+const alphabetUa = "йцукенгшщзхїфівапролджєґячсмитьбю/";
+function addKeyboardLayout(strAlphabet1, strAlphabet2, strAlphabet) {
+const arr = strAlphabet.split('');
+const keyboard = [arr.slice(0, arr.indexOf(strAlphabet1)), arr.slice(arr.indexOf(strAlphabet1), arr.indexOf(strAlphabet2)), arr.slice(arr.indexOf(strAlphabet2))];
     return keyboard;
 }
 const keyboardEn = addKeyboardLayout ("a", "z", alphabetEn);
 const keyboardRu = addKeyboardLayout ("ф", "я", alphabetRu);
 const keyboardUa = addKeyboardLayout ("ф", "я", alphabetUa);
-
 const keyboard = {
     layouts: {
         en: {
@@ -31,30 +30,27 @@ const keyboard = {
     },
     langs: ['en', 'ru', 'ua'],
     currentLang: ''
-};
-
-const writeKeyboardToObj = (keyboardArr, lang) => keyboard.layouts[lang] = keyboardArr;
-
-writeKeyboardToObj (keyboardEn, "en");
-writeKeyboardToObj (keyboardRu, "ru");
-writeKeyboardToObj (keyboardUa, "ua");
-
-const writeLangToObj = (obj) => {
-let lang = "";
+  };
+const keyboardObj = (keyboardArr, langs) => keyboard.layouts[langs] = keyboardArr;
+keyboardObj (keyboardEn, "en");
+keyboardObj (keyboardRu, "ru");
+keyboardObj (keyboardUa, "ua");
+const setLangObj = (obj) => {
+let langs = "";
     do {
-        lang = prompt(' Choose the keyboard layout:  en - 0, ru - 1, ua - 2 ', "");
-    if ( lang === '0' || lang === '1' || lang === '2' ) break;
-    if ( lang === null) return;
-        alert("Был выбран не доступный язык!");
+        langs = +prompt("Выбери язык ", "en-0, ru-1, ua-2");
+    if ( langs === 0 || langs === 1 || langs === 2 ) break;
+    if ( langs === false) return;
+        alert('Выбран недоступный язык');
     } while (true);
-    return obj.currentLang = obj.langs[lang];
+    return obj.currentLang = obj.langs[langs];
 }
-
-writeLangToObj(keyboard);
-
+setLangObj(keyboard);
 const getCurrentKeybArr = (obj) => obj.layouts[obj.currentLang]; 
 
 const currentKeybArr = getCurrentKeybArr(keyboard);
+
+
 
 const getRandCharInAlph = (array) => {
     const randomString = Math.floor(Math.random() * array.length);
