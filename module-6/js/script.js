316ca9c8a20181c4db07423eaec5d8eaec9defdc
Andrey <Andrew0682@ukr.net>
2018-04-02 09:02:03 +0300

    home-6

--- module-6/js/script.js
+++ module-6/js/script.js
@@ -1,63 +1,64 @@
+const keyTrainer = {
+    chars: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'],
+    charCount: "",
+    task: "",
+    userInput: "",
+    userErrors: "",
+    setCharCount() {
+    let howManySymb ="";
+        while (true) {
+            howManySymb = prompt("Введите желаемое количество символов","");
+            if (this.checkPositiveInteger(+howManySymb)) break;
+            if (howManySymb === null) return;
+        } 
+            this.charCount = +howManySymb;    
+    },
+    
+    checkPositiveInteger: function (value) {
+        let isNumInt = (Number.isInteger(value) && value > 0) ? true : false;
+               return isNumInt;
+       
+    },
+    
+    createTask() {
+        if (this.charCount === "") return;
+        let taskArr = new Array(this.charCount);
+        for (let i = 0; i < this.charCount; i++) {
+            taskArr[i] = this.chars[Math.floor(Math.random() * this.chars.length)]; 
+        }  
+        this.task = taskArr;
+                        
+    },
+    
+    startTask() {
+        if (this.charCount === "") return;              
+        const typeStr = prompt(`Наберите следующую строку:     ${this.task.join('')}`);
+        if (typeStr === null ) return;
+        this.userInput = typeStr.split('');
+        
+           
+        const longStr = (this.task.length > this.userInput.length) ? this.task : this.userInput;
+           
+        let errorsCounter = 0;
+        for (let i = 0; i < longStr.length; i++) {
+            if (this.userInput[i] != this.task[i]) {
+                errorsCounter++
+            }
+        } 
+        this.userErrors = errorsCounter;
+           
+        if (this.userErrors === 0) {
+            console.log('Поздравляю!') 
+        } else console.log(`Количество ошибок: ${this.userErrors}.`);
+        
+    },
+    
+};
 
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
+let run = function() {
+    keyTrainer.setCharCount();
+    keyTrainer.createTask();
+    keyTrainer.startTask();
+};
 
-let keyTrainer = {
-  chars: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'],
-  charCount: 0,     
-  setCharCount: function() {
-    let message = prompt("Вам нужно ввести число количества символов: ");
-    while(this.checkPositiveInteger(message)){
-      message = prompt("Вам нужно ввести число: ");
-    }
-    this.charCount = parseInt(message);
-  },
-  checkPositiveInteger: function(a) {
-    if(typeof a !== "number" && a % 1 !== 0 && a !== 0){ 
-      return true;
-    }
-  },
-  task: undefined,
-  createTask: function(){
-    let mass = [];
-    for(var i = 0, size = this.charCount; i < size; i++){    
-      let temp = this.chars[getRandomInt(0, this.chars.length)];
-      mass.push(temp);
-    }
-    this.task = mass;
-  },
-  startTask: function(){
-    let str = this.task.join("");
-    let message = prompt(`Строка task = ${str},  пожалуйста, повторите строку: `);
-    return message;
-  },
-  errors: undefined,
-  userInput: function(){
-    let masInput = this.startTask().split(""); 
-    let genMas = this.task;
-    let count = 0;
-    for(var i = 0, size = this.charCount; i < size; i++){
-      masInput[i] == genMas[i] ? count = count : count++; 
-    }
-    this.errors = count;
-  },
-  userErrors: function(){
-    let errors = this.errors;
-    if(errors === 0){
-      return console.log("Поздравляем у Вас 0 ошибок!!");
-    } else {
-      return console.log(`Колличество ошибок ${errors} error, Успехов в следующей тренировке!`);
-    }
-  },
-  run: function(){
-    this.setCharCount(); 
-    this.createTask();    
-    this.userInput();    
-    return this.userErrors();
-  }
-}
-
-keyTrainer.run();     
-
-
+run(keyTrainer);
