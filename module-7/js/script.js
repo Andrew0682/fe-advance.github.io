fd5b43a6a59276c6fceaeec28e7f943c10ff134a
Andrey <Andrew0682@ukr.net>
2018-04-04 19:07:31 +0300

    home-7

---
+++ module-7/js/script.js
@@ -0,0 +1,67 @@
+const keyboard = {
+    layouts: {
+        en: {
+            topRow: ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]"],
+            middleRow: ["a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'"],
+            bottomRow: ["z", "x", "c", "v", "b", "n", "m", ",", ".", "/"]
+        },
+        ru: {
+            topRow: ["й", "ц", "у", "к", "е", "н", "г", "ш", "щ", "з", "х", "ъ"],
+            middleRow: ["ф", "ы", "в", "а", "п", "р", "о", "л", "д", "ж", "э"],
+            bottomRow: ["я", "ч", "с", "м", "и", "т", "ь", "б", "ю", "."]
+        },
+        ua: {
+            topRow: ["й", "ц", "у", "к", "е", "н", "г", "ш", "щ", "з", "х", "ї"],
+            middleRow: ["ф", "і", "в", "а", "п", "р", "о", "л", "д", "ж", "є"],
+            bottomRow: ["я", "ч", "с", "м", "и", "т", "ь", "б", "ю", "."]
+        }
+    },
+    langs: ["en", "ru", "ua"],
+    currentLang: "null",
+
+    createLayout(row) {
+
+        let rowElement = document.createElement('ul');
+
+        let rowItems = row.map(n => n = '<li></li>')
+            .reduce((acc, next) => acc + next);
+
+        rowElement.insertAdjacentHTML('beforeend', rowItems);
+
+        const elRow = rowElement.querySelectorAll('li');
+        let arrRow = [...elRow];
+        arrRow.map((el, i) => el.textContent = row[i]);
+        return rowElement;
+    },
+    createLayouts() {
+
+        let wrap = document.createElement('div');
+        const keys = document.querySelector("body");
+      
+        const top = this.createLayout(this.layouts[this.langs[this.currentLang]].topRow);
+        const middle = this.createLayout(this.layouts[this.langs[this.currentLang]].middleRow);
+        const bottom = this.createLayout(this.layouts[this.langs[this.currentLang]].bottomRow);
+//сборка элементов
+        wrap.append(top, middle, bottom);
+        keys.append(wrap);
+        return this;
+    },
+//выбор языка
+    setCurrentLang() {
+        return prompt("Выберите язык клавиатуры: en-0, ru-1, ua-2", '');
+    },
+
+    checkPositiveInteger() {
+        let check, langChoise;
+        do {
+            check = this.setCurrentLang();
+        } while (!((+check === 0) || (+check === 1) || (+check === 2)));
+        this.currentLang = +check;
+        return this;
+    }
+};
+
+function run() {
+    keyboard.checkPositiveInteger().createLayouts();
+}
+run();
