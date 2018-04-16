82d6d18a7ebdda41c3394da77fa85b84408fbead
Andrey <Andrew0682@ukr.net>
2018-04-16 13:41:33 +0300

    home-8

--- module-8/script.js
+++ module-8/script.js
@@ -1,3 +1,9 @@
+
+const soundSwitcher = document.querySelector('#slideThree')
+const state = {
+  playSound: soundSwitcher.checked
+}
+
 const playSound = note => {
   const audio = document.querySelector(`audio[data-note=${note}]`);
   audio.currentTime = 0;
@@ -5,31 +11,34 @@ const playSound = note => {
 };
 
 const buttons = Array.from(document.querySelectorAll("button"));
-const keys = " qwertyuiop[]asdfghjkl;'zxcvbnm,./".split("");
 
-let script = window.addEventListener("keydown", input => {
-  let letter = input.key;
+const keys = "qwertyuiop[]asdfghjkl;'zxcvbnm,./ ".split("");
 
-  if(keys.includes(letter)){  
-    if(letter === " "){
-      letter = "space";
-    }
-
-    for(let i = 0; i < buttons.length; i++){
-      if(buttons[i].textContent === letter){
-        buttons[i].classList.add("keyboard__btn--active");
-        console.log(buttons[i])
-        //выбираем ноту в этой букве
-        let sound = buttons[i].getAttribute("data-note");
-        console.log(sound)
-        
-        let checkSound = document.querySelector("#slideThree");
-        if(checkSound.checked){
-          playSound(`${sound}`);
-        }
-      } else{
-        buttons[i].classList.remove("keyboard__btn--active");
-      }
-    }
-   }
+soundSwitcher.addEventListener ('change', (event) => {
+    state.playSound = event.target.checked
+        ? true
+        : false;
 });
+
+const keyboardKeydown = (event) => {
+  if (keys.includes(event.key)) {
+    const targetButton = buttons.find((elem) => {
+      return elem.textContent == event.key;
+    })
+
+    targetButton.classList.add('keyboard__btn--active');
+    let note = targetButton.getAttribute("data-note");
+    if (state.playSound) playSound(note);
+  }
+}
+
+const keyboardKeyup = (event) => {
+  const target = document.querySelectorAll('.keyboard__btn--active');
+
+  target.forEach((element) => {
+    element.classList.remove('keyboard__btn--active');
+  });
+}
+
+window.addEventListener("keydown", keyboardKeydown);
+window.addEventListener("keyup", keyboardKeyup);
