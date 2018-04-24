9350eec108a42dfa6663164b3f74792530af2337
Andrey <Andrew0682@ukr.net>
2018-04-24 16:23:07 +0300

    home-10

--- module-10/script.js
+++ module-10/script.js
@@ -108,14 +108,14 @@ function announcement() {
 
 
 function getChar(event) {
-  if (event.which == null) { // IE
-    if (event.keyCode < 32) return null; // спец. символ
+  if (event.which == null) { 
+    if (event.keyCode < 32) return null; 
     return String.fromCharCode(event.keyCode)
   }
 
-  if (event.which != 0 && event.charCode != 0) { // все кроме IE
-    if (event.which < 32) return null; // спец. символ
-    return String.fromCharCode(event.which); // остальные
+  if (event.which != 0 && event.charCode != 0) { 
+    if (event.which < 32) return null; 
+    return String.fromCharCode(event.which); 
   }
 
   return null; // спец. символ
